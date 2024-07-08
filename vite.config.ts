ec7bffea0feb9270137fcff03abe85199969a2de
Hanna Lisitcyna <104447922+lisitsyna-anna@users.noreply.github.com>
2024-07-08 19:56:33 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/vite-react-template/',
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/__tests__/setup.ts'],
+  },
+});
